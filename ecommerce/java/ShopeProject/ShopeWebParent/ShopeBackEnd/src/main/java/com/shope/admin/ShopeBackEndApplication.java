42f0d7499fb8849673e8f0dda191a92fa74e6c73
ms <soe3676@nate.com>
2023-02-06 15:55:12 +0900

    start

---
+++ ecommerce/java/ShopeProject/ShopeWebParent/ShopeBackEnd/src/main/java/com/shope/admin/ShopeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shope.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeBackEndApplication.class, args);
+	}
+
+}
